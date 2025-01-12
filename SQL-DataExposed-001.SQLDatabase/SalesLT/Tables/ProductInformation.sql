943d38636c4627eac6ddf5df8ebee1897551b035
Hamish Watson <30678414+HeyMo0sh@users.noreply.github.com>
2025-01-12 19:38:12 +1300

    Committing 1 item from workspace e1905f40-8890-4281-b23d-f3661f2af7ce

--- SQL-DataExposed-001.SQLDatabase/SalesLT/Tables/ProductInformation.sql
+++ SQL-DataExposed-001.SQLDatabase/SalesLT/Tables/ProductInformation.sql
@@ -1,16 +1,21 @@
-CREATE TABLE [SalesLT].[ProductInformation] ( 
-    [ProductID] INT IDENTITY(1,1) PRIMARY KEY, -- Primary key, auto-incrementing
-    [ProductName] VARCHAR(100) NOT NULL,       -- Name of the product
-    [Description] VARCHAR(MAX),                -- Description of the product
-    [Brand] VARCHAR(50),                       -- Brand of the product
-    [ListPrice] DECIMAL(10, 2),                -- List price of the product
-    [SalePrice] DECIMAL(10, 2),                -- Sale price of the product
-    [ItemNumber] VARCHAR(20),                  -- Item number of the product
-    [GTIN] VARCHAR(20),                        -- Global Trade Item Number
-    [PackageSize] VARCHAR(50),                 -- Size of the package
-    [Category] VARCHAR(50),                    -- Category of the product
-    [PostalCode] VARCHAR(10),                  -- Postal code related to the product
-    [Available] BIT,                           -- Availability status (1 for available, 0 for not available)
-    [Embedding] VARBINARY(MAX),                -- Binary data for embedding
-    [CreateDate] DATETIME                      -- Date the record was created
+CREATE TABLE [SalesLT].[ProductInformation] (
+    [ProductID]   INT             IDENTITY (1, 1) NOT NULL,
+    [ProductName] VARCHAR (100)   NOT NULL,
+    [Description] VARCHAR (MAX)   NULL,
+    [Brand]       VARCHAR (50)    NULL,
+    [ListPrice]   DECIMAL (10, 2) NULL,
+    [SalePrice]   DECIMAL (10, 2) NULL,
+    [ItemNumber]  VARCHAR (20)    NULL,
+    [GTIN]        VARCHAR (20)    NULL,
+    [PackageSize] VARCHAR (50)    NULL,
+    [Category]    VARCHAR (50)    NULL,
+    [PostalCode]  VARCHAR (10)    NULL,
+    [Available]   BIT             NULL,
+    [Embedding]   VARBINARY (MAX) NULL,
+    [CreateDate]  DATETIME        NULL,
+    PRIMARY KEY CLUSTERED ([ProductID] ASC)
 );
+
+
+GO
+
