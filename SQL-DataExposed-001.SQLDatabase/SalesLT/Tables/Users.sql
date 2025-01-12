18ad0385c7e5ed370e47805eabd101fc91a40cc5
Hamish Watson <30678414+HeyMo0sh@users.noreply.github.com>
2025-01-12 22:19:52 +1300

    Committing 1 item from workspace e1905f40-8890-4281-b23d-f3661f2af7ce

--- SQL-DataExposed-001.SQLDatabase/SalesLT/Tables/Users.sql
+++ SQL-DataExposed-001.SQLDatabase/SalesLT/Tables/Users.sql
@@ -1,12 +1,15 @@
 CREATE TABLE [SalesLT].[Users] (
-    [UserID] INT IDENTITY(1,1) PRIMARY KEY, -- Primary key, auto-incrementing
-    [Username] NVARCHAR(50) NOT NULL,       -- Username of the user
-    [PasswordHash] NVARCHAR(255) NOT NULL,  -- Hashed password of the user
-    [Email] NVARCHAR(100) NOT NULL,         -- Email of the user
-    [FirstName] NVARCHAR(50) NULL,          -- First name of the user
-    [LastName] NVARCHAR(50) NULL,           -- Last name of the user
-    [CreatedDate] DATETIME DEFAULT GETDATE(), -- Date the user was created
-    [LastLoginDate] DATETIME NULL           -- Date the user last logged in
+    [UserID]        INT            IDENTITY (1, 1) NOT NULL,
+    [Username]      NVARCHAR (50)  NOT NULL,
+    [PasswordHash]  NVARCHAR (255) NOT NULL,
+    [Email]         NVARCHAR (100) NOT NULL,
+    [FirstName]     NVARCHAR (50)  NULL,
+    [LastName]      NVARCHAR (50)  NULL,
+    [CreatedDate]   DATETIME       DEFAULT (getdate()) NULL,
+    [LastLoginDate] DATETIME       NULL,
+    PRIMARY KEY CLUSTERED ([UserID] ASC)
 );
 
+
 GO
+
